95991a82eb80795a4982c6aa8f2bbfe560452ea3
Henrik Schmidt-Møller <henrikm@tlt.local>
2017-01-18 10:58:19 +0400

    added rewire test

---
+++ app/app.js
@@ -0,0 +1,3 @@
+const sanity = function () {
+  return true;
+};
--- test/test.js
+++ test/test.js
@@ -1,10 +1,20 @@
+const rewire = require('rewire');
 const expect = require("chai").expect;
-const app = require("../app/app");
+
+//const app = require("../app/app");
+const app = rewire('../app/app');
 
 describe('Test Suite', function () {
   describe('Sanity test', function () {
     it('checks that true equals true', function () {
       expect(true).to.equal(true);
+    });
+  });
+
+  const sanity = app.__get__('sanity');
+  describe('Sanity test from app.js', function () {
+    it('checks that true equals true', function () {
+      expect(true).to.equal(sanity());
     });
   });
 });
